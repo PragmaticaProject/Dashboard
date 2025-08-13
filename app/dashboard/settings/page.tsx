7fa3f13fbdf0c1f253cf45df4789657cf5c562aa
Karthik Prasad <karthikprasad62@gmail.com>
2025-08-12 22:40:05 -0700

    deprecate user settings (empty page)

--- app/dashboard/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return(
-        <div className="flex flex-col mx-auto items-center">
-            <h1 className="text-4xl font-bold">Pragmatica Settings Page</h1>
-        </div>
-    );
-}
